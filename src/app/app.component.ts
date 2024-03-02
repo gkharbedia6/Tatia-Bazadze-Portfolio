7f1fca5cf9238b416dad719ba7ce03c9726f0a2d
rollmemaybe6 <giorgikharbedia6@gmail.com>
2024-03-02 21:15:57 +0400

    bootstraped angular standalone component project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { ImagesCollagesComponent } from 'src/components/images-collage.component';
 
 @Component({
   selector: 'app-root',
-  template: ` <div class="bg-red-500 w-56 h-56">
-    <div>fdfd</div>
+  standalone: true,
+  template: ` <div
+    class="min-h-screen h-full w-full flex flex-col items-start justify-start"
+  >
+    <div class="w-full h-[30%] bg-gray-500">grgrg</div>
+    <div class="w-full min-h-screen">
+      <images-collages></images-collages>
+    </div>
   </div>`,
+  imports: [ImagesCollagesComponent],
 })
 export class AppComponent {
   title = 'tatia-bazadze-portfolio';
