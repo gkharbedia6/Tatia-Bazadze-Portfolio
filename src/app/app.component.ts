2cce39fd35d2a11948dbb56d10eaacd0a5ec3fa8
rollmemaybe6 <giorgikharbedia6@gmail.com>
2024-03-02 21:01:51 +0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: ` <div class="bg-red-500 w-56 h-56">
+    <div>fdfd</div>
+  </div>`,
 })
 export class AppComponent {
   title = 'tatia-bazadze-portfolio';
